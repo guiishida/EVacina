2b0647693869d6704fb6a8e563711cb8f91f064c
gusgreco <gustavo.greco.85@gmail.com>
2020-05-13 16:37:46 -0300

    Working Scan - shows value on screen, no post

--- app/src/main/java/com/example/evacina/MainMenuActivity.java
+++ app/src/main/java/com/example/evacina/MainMenuActivity.java
@@ -3,24 +3,70 @@ package com.example.evacina;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.android.material.textfield.TextInputLayout;
 
-public class MainMenuActivity extends AppCompatActivity {
+public class MainMenuActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText editTextLogin;
     TextInputLayout layout;
+    Button buttonNewVaccine, buttonVisualize, buttonGeneratePDF, buttonGenerateCertificates, buttonMaps, buttonPendingVaccines;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         ActionBar actionBar = getSupportActionBar();
-        if(actionBar != null){
+        if (actionBar != null) {
             actionBar.setDisplayShowHomeEnabled(true);
             actionBar.setTitle(R.string.user_main_menu);
         }
         setContentView(R.layout.activity_main_menu);
+
+        initViews();
+
+
+
+        buttonNewVaccine.setOnClickListener(this);
+        buttonVisualize.setOnClickListener(this);
+        buttonGeneratePDF.setOnClickListener(this);
+        buttonGenerateCertificates.setOnClickListener(this);
+        buttonMaps.setOnClickListener(this);
+        buttonPendingVaccines.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.buttonNewVaccine:
+                Intent intent = new Intent(MainMenuActivity.this, ScanActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.buttonVisualize:
+                break;
+            case R.id.buttonGeneratePDF:
+                break;
+            case R.id.buttonGenerateCertificates:
+                break;
+            case R.id.buttonMaps:
+                break;
+            case R.id.buttonPendingVaccines:
+                break;
+        }
+    }
+
+    private void initViews() {
+        buttonNewVaccine = findViewById(R.id.buttonNewVaccine);
+        buttonVisualize = findViewById(R.id.buttonVisualize);
+        buttonGeneratePDF = findViewById(R.id.buttonGeneratePDF);
+        buttonGenerateCertificates = findViewById(R.id.buttonGenerateCertificates);
+        buttonMaps = findViewById(R.id.buttonMaps);
+        buttonPendingVaccines = findViewById(R.id.buttonPendingVaccines);
+    }
+
 }
---
+++ app/src/main/java/com/example/evacina/ScanActivity.java
@@ -0,0 +1,49 @@
+package com.example.evacina;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class ScanActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA}, PackageManager.PERMISSION_GRANTED);
+
+        textView = findViewById(R.id.textView2);
+
+        IntentIntegrator intentIntegrator = new IntentIntegrator(this);
+        intentIntegrator.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+
+        if (intentResult != null) {
+            if (intentResult.getContents() == null) {
+                Toast.makeText(ScanActivity.this, "Scan Error", Toast.LENGTH_SHORT).show();
+            } else {
+                textView.setText(intentResult.getContents());
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+}
