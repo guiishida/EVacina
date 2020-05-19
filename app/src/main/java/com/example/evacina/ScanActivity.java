b1d790c784f1ac04088a97ef0d3c18d5497694b5
gusgreco <gustavo.greco.85@gmail.com>
2020-05-19 18:41:56 -0300

    Location obtained before scan

--- app/src/main/java/com/example/evacina/ScanActivity.java
+++ app/src/main/java/com/example/evacina/ScanActivity.java
@@ -4,6 +4,9 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,8 +17,10 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.example.evacina.androidloginregisterrestfullwebservice.ApiUtils;
+import com.example.evacina.androidloginregisterrestfullwebservice.PermissionUtils;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
@@ -27,7 +32,8 @@ import retrofit2.Response;
 import com.example.evacina.androidloginregisterrestfullwebservice.VaccineService;
 import com.example.evacina.androidloginregisterrestfullwebservice.VaccineRegisterResponseObjectModel;
 
-public class ScanActivity extends AppCompatActivity {
+
+public class ScanActivity extends AppCompatActivity implements LocationListener {
 
     TextView textViewInstructions;
     VaccineService vaccineService;
@@ -35,6 +41,16 @@ public class ScanActivity extends AppCompatActivity {
     Button buttonScan;
 
     private String Email;
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    // The minimum distance to change Updates in meters
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
+    // The minimum time between updates in milliseconds
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60; // 1 minute
+    protected LocationManager locationManager;
+    private Location location;
+    private double latitude; // latitude
+    private double longitude; // longitude
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,23 +62,62 @@ public class ScanActivity extends AppCompatActivity {
             actionBar.setTitle(R.string.registrar_vacina);
         }
 
+
         Email = getIntent().getStringExtra("email");
+
         setContentView(R.layout.activity_scan);
 
         final Activity activity = this;
         initViews();
+        getLocation();
 
         buttonScan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ActivityCompat.requestPermissions(activity,new String[]{Manifest.permission.CAMERA}, PackageManager.PERMISSION_GRANTED);
+                ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.CAMERA}, PackageManager.PERMISSION_GRANTED);
                 intentIntegrator = new IntentIntegrator(activity);
                 intentIntegrator.setBeepEnabled(false);
                 intentIntegrator.initiateScan();
             }
         });
+    }
 
+    private void getLocation() {
+        locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
 
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER,
+                    MIN_TIME_BW_UPDATES,
+                    MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+            if (locationManager != null) {
+                location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                if (location != null) {
+                    latitude = location.getLatitude();
+                    longitude = location.getLongitude();
+                } else {
+                    locationManager.requestLocationUpdates(
+                            LocationManager.GPS_PROVIDER,
+                            MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                    if (locationManager != null) {
+                        location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        if (location != null) {
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+                        }
+                    }
+                }
+            }
+        } else {
+            // Permission to access the location is missing. Show rationale and request permission
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true);
+        }
+        if (locationManager != null) {
+            locationManager.removeUpdates(this);
+        }
     }
 
     private void initViews() {
@@ -85,6 +140,9 @@ public class ScanActivity extends AppCompatActivity {
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
+
+
+
 
     private void doRegister(final Long code) {
         Call<VaccineRegisterResponseObjectModel> call = vaccineService.register(code, Email);
@@ -119,5 +177,24 @@ public class ScanActivity extends AppCompatActivity {
                 Toast.makeText(ScanActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        // TODO Auto-generated method stub
+    }
+    @Override
+    public void onProviderDisabled(String provider) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        // TODO Auto-generated method stub
     }
 }
